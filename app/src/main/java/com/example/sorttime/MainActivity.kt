7e31b082612da712bcc5c10f0fde82f28e4fde79
Jamie <ishighjamie@gmail.com>
2022-12-23 16:08:44 +0100

    Deklaracje

--- app/src/main/java/com/example/sorttime/MainActivity.kt
+++ app/src/main/java/com/example/sorttime/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.sorttime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lisBeftex = findViewById<TextView>(R.id.textView6)
+        val lisAfttex = findViewById<TextView>(R.id.textView7)
+        val sorBab = findViewById<TextView>(R.id.textView)
+        val sorWst = findViewById<TextView>(R.id.textView2)
+        val sorSca = findViewById<TextView>(R.id.textView3)
+        val sorSzy = findViewById<TextView>(R.id.textView4)
+        val sorHea = findViewById<TextView>(R.id.textView5)
+        val lenLis = findViewById<EditText>(R.id.editTextTextPersonName)
+        val sorIlo = findViewById<EditText>(R.id.editTextTextPersonName2)
+        val trig = findViewById<Button>(R.id.button)
+
     }
 }
