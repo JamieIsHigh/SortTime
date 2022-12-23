ae289650f6a49c25bcd57cd7094cc02d22ceacfe
Jamie <ishighjamie@gmail.com>
2022-12-23 20:11:17 +0100

    Mergesort

--- app/src/main/java/com/example/sorttime/MainActivity.kt
+++ app/src/main/java/com/example/sorttime/MainActivity.kt
@@ -79,6 +79,45 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        fun merge(arr: IntArray, left: IntArray, right: IntArray) {
+            var i = 0
+            var j = 0
+            var k = 0
+            while (i < left.size && j < right.size) {
+                if (left[i] < right[j]) {
+                    arr[k] = left[i]
+                    i++
+                } else {
+                    arr[k] = right[j]
+                    j++
+                }
+                k++
+            }
+            while (i < left.size) {
+                arr[k] = left[i]
+                i++
+                k++
+            }
+            while (j < right.size) {
+                arr[k] = right[j]
+                j++
+                k++
+            }
+        }
+
+        fun mergeSort(arr: IntArray) {
+            if (arr.size > 1) {
+                val mid = arr.size / 2
+                val left = arr.copyOfRange(0, mid)
+                val right = arr.copyOfRange(mid, arr.size)
+
+                mergeSort(left)
+                mergeSort(right)
+
+                merge(arr, left, right)
+            }
+        }
+
         trig.setOnClickListener {
             var dlugosc = 1
             if (lenLis.length() > 0) {
