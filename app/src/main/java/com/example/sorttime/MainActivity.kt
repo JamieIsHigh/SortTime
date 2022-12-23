bc80217ae37d1c89f633bd9b4e5346f1e08aca1c
Jamie <ishighjamie@gmail.com>
2022-12-23 18:02:05 +0100

    Poprawa sortowania + generowanie listy

--- app/src/main/java/com/example/sorttime/MainActivity.kt
+++ app/src/main/java/com/example/sorttime/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //deklaracja elementów layoutu
+        val random = Random()
         val lisBeftex = findViewById<TextView>(R.id.textView6)
         val lisAfttex = findViewById<TextView>(R.id.textView7)
         val sorBab = findViewById<TextView>(R.id.textView)
@@ -22,16 +26,26 @@ class MainActivity : AppCompatActivity() {
         val sorIlo = findViewById<EditText>(R.id.editTextTextPersonName2)
         val trig = findViewById<Button>(R.id.button)
 
-        fun bubSor(lis: IntArray) {
-            for (i in lis.indices) {
-                for (x in 0 until lis.size - i - 1) {
-                    if (lis[x] > lis[x + 1]) {
-                        val temp = lis[x]
-                        lis[x] = lis[x + 1]
-                        lis[x + 1] = temp
+        fun bubbleSort(arr:IntArray):IntArray{
+            var swap = true
+            while(swap){
+                swap = false
+                for(i in 0 until arr.size-1){
+                    if(arr[i] > arr[i+1]){
+                        val temp = arr[i]
+                        arr[i] = arr[i+1]
+                        arr[i + 1] = temp
+
+                        swap = true
                     }
                 }
             }
+            return arr
+        }
+        //wciśnięcie przycisku
+        trig.setOnClickListener {
+            var dlugosc = lenLis.text.toString().toInt()
+            var lista = IntArray(dlugosc) { random.nextInt(10) }
         }
     }
 }
